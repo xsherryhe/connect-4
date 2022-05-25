962cade2e581040b1398e0765456b6343edf8238
Sherry He <xsherryhe@gmail.com>
2022-05-24 20:11:17 -0400

    Completed test writing for #take_turn method of Game class

--- spec/connect_4_game_spec.rb
+++ spec/connect_4_game_spec.rb
@@ -2,10 +2,12 @@ require_relative '../lib/connect_4_game.rb'
 
 describe Game do
   subject(:game) { described_class.new }
+  let(:board) { game.instance_variable_get(:@board) }
+  let(:expected_board) { [['   '] * 7] * 6 }
 
   before do
-    john = instance_double(Player, name: 'John', marker: "\u25EF")
-    mary = instance_double(Player, name: 'Mary', marker: "\u2B24")
+    john = instance_double(Player, name: 'John', marker: "\e[31m\u2B24\e[0m")
+    mary = instance_double(Player, name: 'Mary', marker: "\e[34m\u2B24\e[0m")
     allow(Player).to receive(:new).and_return(john, mary)
   end
 
@@ -16,12 +18,16 @@ describe Game do
     end
 
     it 'creates a board with an array of 6 rows and 7 columns' do
-      board = game.instance_variable_get(:@board)
-      expect(board).to eq([['   '] * 7] * 6)
+      expect(board).to eq(expected_board)
     end
   end
 
   describe '#play' do
+    before do
+      allow(game).to receive(:take_turn)
+      allow(game).to receive(:evaluate_game_over)
+    end
+
     context 'when the game is over' do
       it 'does not execute the loop' do
         game.instance_variable_set(:@game_over, true)
@@ -57,36 +63,103 @@ describe Game do
     end
 
     it 'outputs instruction to current player' do
-      expect(game).to receive(:puts).with(/John, please select a column to place your token./)
+      expect(game).to receive(:puts).with(/John, please enter a column number to place your token./)
       game.take_turn
     end
 
     it 'outputs error message while input is not a number' do
-      
+      allow(game).to receive(:gets).and_return('d', '@', '*', '(()))', 'hello world', '5')
+      expect(game).to receive(:puts).with(/Please enter a column number between 1 and 7./).exactly(5).times
+      game.take_turn
     end
 
     it 'outputs error message while input is out of range' do
-      
+      allow(game).to receive(:gets).and_return('24', '0', '8', '5')
+      expect(game).to receive(:puts).with(/Please enter a column number between 1 and 7./).exactly(3).times
+      game.take_turn
     end
 
     it 'outputs error message while the selected column is full' do
-      
+      game.instance_variable_set(:@board, [['   '] * 6 + ['X']] * 6)
+      allow(game).to receive(:gets).and_return('7', '6')
+      expect(game).to receive(:puts).with(/The column you selected is full. Please select a different column number./).once
+      game.take_turn
     end
 
     it 'outputs the appropriate error message depending on the error' do
-      
+      game.instance_variable_set(:@board, [['X'] + ['   '] * 6] * 6)
+      allow(game).to receive(:gets).and_return('1', '1', '1', 'foo', '37', '5')
+      expect(game).to receive(:puts).with(/The column you selected is full. Please select a different column number./).exactly(3).times
+      expect(game).to receive(:puts).with(/Please enter a column number between 1 and 7./).twice
+      game.take_turn
     end
 
-    it 'returns an input that is a valid column number' do
-      
+    context 'when the selected column is a middle column' do
+      it 'fills a slot in the selected column number' do
+        allow(game).to receive(:gets).and_return('5')
+        game.take_turn
+        expected_board[5][4] = "\e[31m\u2B24\e[0m"
+        expect(board).to eq(expected_board)
+      end
     end
 
-    it 'fills one more slot in the selected column' do
-      
+    context 'when the selected column is the first column' do
+      it 'fills a slot in the first column' do
+        allow(game).to receive(:gets).and_return('1')
+        game.take_turn
+        expected_board[5][0] = "\e[31m\u2B24\e[0m"
+        expect(board).to eq(expected_board)
+      end
     end
 
-    it 'switches current player to next player' do
-      
+    context 'when the selected column is the last column' do
+      it 'fills a slot in the last column' do
+        allow(game).to receive(:gets).and_return('7')
+        game.take_turn
+        expected_board[5][6] = "\e[31m\u2B24\e[0m"
+        expect(board).to eq(expected_board)
+      end
+    end
+
+    context 'when the selected column is partly filled' do
+      it 'fills the next slot in the selected column' do
+        expected_board.last(2).each { |row| row[3] = 'X' }
+        game.instance_variable_set(:@board, expected_board)
+        allow(game).to receive(:gets).and_return('4')
+        game.take_turn
+        expected_board[3][3] = "\e[31m\u2B24\e[0m"
+        expect(board).to eq(expected_board)
+      end
+    end
+
+    it 'switches next player to current player' do
+      game.take_turn
+      index = game.instance_variable_get(:@curr_player_index)
+      expect(index).to eq(1)
+    end
+
+    context 'when the current player is the second player' do
+      before do
+        game.instance_variable_set(:@curr_player_index, 1)
+      end
+
+      it "outputs instruction with the correct current player's name" do
+        expect(game).to receive(:puts).with(/Mary, please enter a column number to place your token./)
+        game.take_turn
+      end
+
+      it "fills the slot with the correct current player's marker" do
+        allow(game).to receive(:gets).and_return('6')
+        game.take_turn
+        expected_board[5][5] = "\e[34m\u2B24\e[0m"
+        expect(board).to eq(expected_board)
+      end
+
+      it 'switches next player to current player' do
+        game.take_turn
+        index = game.instance_variable_get(:@curr_player_index)
+        expect(index).to eq(0)
+      end
     end
   end
 
